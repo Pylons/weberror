532ff704b2beceee9dd5d273ff8e90818075e687
Ian Bicking <ianb@colorstudy.com>
2007-09-22 14:12:38 -0500

    demo to play around with the evalexception interface itself

---
+++ tests/evaldemo.py
@@ -0,0 +1,15 @@
+from weberror.evalexception import EvalException
+
+def error_application(environ, start_response):
+    a = 1
+    b = 'x'*1000
+    return sub_application(environ, start_response)
+
+def sub_application(environ, start_response):
+    test = 10
+    raise Exception('The expected error')
+
+if __name__ == '__main__':
+    from paste.httpserver import serve
+    serve(EvalException(error_application))
+    
