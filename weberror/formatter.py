dfd6169a9ed9ac49f6dc5ce38b37c21aed39fe22
ianb@colorstudy.com <ianb@colorstudy.com>
2008-06-30 16:13:47 -0500

    Add the highlight class to formatted source

--- weberror/formatter.py
+++ weberror/formatter.py
@@ -301,7 +301,7 @@ class HTMLFormatter(TextFormatter):
     def format_long_source(self, source, long_source):
         q_long_source = str2html(long_source, False, 4, True, getattr(self, 'frame', None))
         q_source = str2html(source, True, 0, False, getattr(self, 'frame', None))
-        return ('<div style="display: none" class="source" source-type="long"><a class="switch_source" onclick="return switch_source(this, \'long\')" href="#">&lt;&lt;&nbsp; </a>%s</div>'
+        return ('<div style="display: none" class="source highlight" source-type="long"><a class="switch_source" onclick="return switch_source(this, \'long\')" href="#">&lt;&lt;&nbsp; </a>%s</div>'
                 '<div class="source" source-type="short"><a onclick="return switch_source(this, \'short\')" class="switch_source" href="#">&gt;&gt;&nbsp; </a>%s</div>'
                 % (q_long_source,
                    q_source))
